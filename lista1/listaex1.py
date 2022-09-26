8d681d5f988a705f33378f8b4cc99d4ca78567e5
Jajcmacedo <113530763+Jajcmacedo@users.noreply.github.com>
2022-09-26 16:35:51 -0300

    Update listaex1.py
    Signed-off-by: Jajcmacedo <113530763+Jajcmacedo@users.noreply.github.com>

--- lista1/listaex1.py
+++ lista1/listaex1.py
@@ -1,11 +1,11 @@
-l = int(input('Digite até onde a sequencia de Fibonacci deva ir:'))
+L = int(input('Digite até onde a sequencia de Fibonacci deva ir:'))
 t1 = 1
-if t1 < l:
+if t1 < L:
     print(t1)
 t2 = 1
-if t2 < l:
+if t2 < L:
     print(t2)
-while (t1 + t2) <= l:
+while (t1 + t2) <= L:
     t = t1 + t2
     print(t)
     t1 = t2
