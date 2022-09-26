0aa382feb3ee96fafab21736f3c7842ab3effb9d
Jajcmacedo <113530763+Jajcmacedo@users.noreply.github.com>
2022-09-26 15:27:03 -0300

    Delete ex3.py
    Signed-off-by: Jajcmacedo <113530763+Jajcmacedo@users.noreply.github.com>

--- ex3.py
+++
@@ -1,8 +0,0 @@
-n = int(input('Informe um número de 5 dígitos para descobrirmos se é um palíndromo:'))
-if len(str(n)) == 5:
-    if str(n) == str(n)[::-1]:
-        print('É um palíndromo')
-    else:
-        print('Não é um palíndromo')
-else:
-    print('O número precisa conter 5 dígitos!')
