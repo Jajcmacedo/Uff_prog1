e5f482cdfa01ede8d3e9f513cc6309ba94ec98fe
Jajcmacedo <113530763+Jajcmacedo@users.noreply.github.com>
2022-09-26 12:41:15 -0300

    Update listaex4.py
    Signed-off-by: Jajcmacedo <113530763+Jajcmacedo@users.noreply.github.com>

--- lista1/listaex4.py
+++ lista1/listaex4.py
@@ -2,7 +2,6 @@ x = int(input('Informe um valor para X:'))
 while x != 0:
     if x % 2 == 0:
         print(x + (x + 2) + (x + 4) + (x + 6) + (x + 8))
-        break
     else:
         print((x + 1) + (x + 3) + (x + 5) + (x + 7) + (x + 9))
-        break
+    x = int(input('Informe um valor para X:'))
